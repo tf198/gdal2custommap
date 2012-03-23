68c6090c815036d037fb0f6e648fad7042a1ff91
Tris Forster <tris.git@tfconsulting.com.au>
2012-03-23 16:04:11 +1100

    Switched from command line subprocess to proper python bindings

--- gdal2kml.py
+++ gdal2kml.py
@@ -5,7 +5,7 @@ from osgeo import gdal
 def tiles(canvas, target=1024):
 	""" 
 	Brute force algorithm to determine the most efficient tiling method for a given canvas
-	If anyone can figure out a prettier one please let me know
+	If anyone can figure out a prettier one please let me know...
 	"""
 	best_case = (canvas[0] * canvas[1]) / float(target**2)
 	
@@ -31,12 +31,40 @@ def tiles(canvas, target=1024):
 	results.sort(key=lambda x: x[2])
 	return [ int(x) for x in results[0][0:2] ]
 
+def create_tile(source, filename, offset, size):
+	mem_drv = gdal.GetDriverByName('MEM')
+	mem_ds = mem_drv.Create('', size[0], size[1], source.RasterCount)
+	bands = range(1, source.RasterCount+1)
+
+	data = source.ReadRaster(offset[0], offset[1], size[0], size[1], size[0], size[1], band_list=bands)
+	mem_ds.WriteRaster(0, 0, size[0], size[1], data, band_list=bands)
+
+	jpeg_drv = gdal.GetDriverByName('JPEG')
+	jpeg_ds = jpeg_drv.CreateCopy(filename, mem_ds, strict=0)
+
+	t = source.GetGeoTransform()
+	def transform((x, y)):
+		return ( t[0] + x*t[1] + y*t[2], t[3] + x*t[4] + y*t[5] )
+	
+	nw = transform(offset)
+	se = transform([ offset[0] + size[0], offset[1] + size[1] ])
+	
+	result = {
+		'north': nw[1],
+		'east': se[0],
+		'south': se[1],
+		'west': nw[0],
+	}
+	
+	jpeg_ds = None
+	mem_ds = None
+	
+	return result
+	
 usage = "usage: %prog [options] src_file dst_file"
 parser = OptionParser(usage)
-parser.add_option('-s', '--scale', default=100, dest='scale', type='int', help='A scale percentage for output [10-150]')
 parser.add_option('-d', '--dir', dest='working', help='Where to create jpeg tiles')
 parser.add_option('-c', '--crop', default=0, dest='border', type='int', help='Crop border')
-#parser.add_option('-1', '--single', action='store_true', dest='single', help='Force single scaled tile generation')
 parser.add_option('-n', '--name', dest='name', help='KML folder name for output')
 parser.add_option('-o', '--draw-order', dest='order', type='int', default=20, help='KML draw order')
 parser.add_option('-t', '--tile-size', dest='tile_size', default=1024, type='int', help='Max tile size [1024]')
@@ -51,31 +79,23 @@ if options.verbose: logging.basicConfig(level=logging.DEBUG)
 
 # validate a few options
 if not os.path.exists(source): parser.error('unable to file src_file')
-if options.scale<10 or options.scale>150: parser.error('scale must be between 10% and 150%')
+#if options.scale<10 or options.scale>150: parser.error('scale must be between 10% and 150%')
 
 # set the default folder for jpegs
 if not options.working: options.working = "%s.files" % os.path.splitext(dest)[0]
 logging.info('Writing jpegs to %s' % options.working)
 
-#print options
-
-nw = re.compile(r'Upper Left\s+\( (\-?[0-9\.]+), (\-?[0-9\.]+)\)')
-se = re.compile(r'Lower Right\s+\( (\-?[0-9\.]+), (\-?[0-9\.]+)\)')
-size = re.compile(r'Size is (\-?[0-9\.]+), (\-?[0-9\.]+)')
-
 img = gdal.Open(source);
 img_size = [img.RasterXSize, img.RasterYSize]
-img = None
+
 logging.debug('Image size: %s' % img_size)
 
 cropped_size = [ x - options.border * 2 for x in img_size ]
 
-factor = float(options.tile_size * 100 / options.scale)
-
 base, ext = os.path.splitext(os.path.basename(source))
 
 if not options.name: options.name = base
-if not options.order: options.order = options.scale / 5
+if not options.order: options.order = 20
 
 if not os.path.exists(options.working): os.mkdir(options.working)
 path = os.path.relpath(options.working, os.path.dirname(dest))
@@ -88,11 +108,11 @@ logging.debug('Using tile layout %s -> %s' % (tile_layout, tile_sizes))
 # load the exclude file
 exclude_file = source + ".exclude"
 exclude = []
-if options.scale == 100 and os.path.exists(exclude_file):
+if os.path.exists(exclude_file):
   logging.debug("Using exclude file %s" % exclude_file)
   for line in open(exclude_file):
     exclude.append(line.rstrip())
-  #logging.debug(exclude)
+  logging.debug(exclude)
 
 bob = open(dest, 'w')
 	
@@ -105,7 +125,7 @@ bob.write("""<?xml version="1.0" encoding="UTF-8"?>
 for t_y in range(tile_layout[1]):
   for t_x in range(tile_layout[0]):
     tile = "%d,%d" % (t_y, t_x)
-    #logging.debug(tile)
+    logging.debug(tile)
     if tile in exclude:
       logging.debug("Excluding tile %s" % tile)
     else:
@@ -113,22 +133,10 @@ for t_y in range(tile_layout[1]):
       src_size = [tile_sizes[0], tile_sizes[1]]
       if src_corner[0] + tile_sizes[0] > img_size[0] - options.border: src_size[0] = int(tile_sizes[0])
       if src_corner[1] + tile_sizes[1] > img_size[1] - options.border: src_size[1] = int(tile_sizes[1])
-      #if options.single: src_size = cropped_size
       
-      outfile = "%s_%d_%d_%d.jpg" % (base, options.scale, t_x, t_y)
-      out_scale = "%d%%" % options.scale
-      cmd = [
-      'gdal_translate', 
-      '-of', 'JPEG', 
-      '-srcwin', src_corner[0], src_corner[1], src_size[0], src_size[1],
-      '-outsize', out_scale, out_scale,
-      '-q',
-      source,
-      '%s/%s' % (options.working, outfile)
-      ]
-      cmd = [ str(x) for x in cmd ]
-      subprocess.check_call(cmd)
-      
+      outfile = "%s_%d_%d.jpg" % (base, t_x, t_y)
+      bounds = create_tile(img, "%s/%s" % (options.working, outfile), src_corner, src_size)
+			
       bob.write("""    <GroundOverlay>
       <name>%s</name>
       <color>ffffffff</color>
@@ -139,20 +147,13 @@ for t_y in range(tile_layout[1]):
       </Icon>
       <LatLonBox>
 """ % (outfile, options.order, path, outfile))
-    # get the bounds
-      img = gdal.Open("%s/%s" %(options.working, outfile))
-      wf = img.GetGeoTransform()
-      north = wf[3]
-      west = wf[0]
-      east = west + (wf[1] * img.RasterXSize)
-      south = north + (wf[5] * img.RasterYSize)
-      img = None
-      bob.write("""        <north>%s</north>
-        <south>%s</south>
-        <east>%s</east>
-        <west>%s</west>
+    
+      bob.write("""        <north>%(north)s</north>
+        <south>%(south)s</south>
+        <east>%(east)s</east>
+        <west>%(west)s</west>
         <rotation>0</rotation>
-""" % (north,south, east, west))
+""" % bounds)
       bob.write("""        </LatLonBox>
     </GroundOverlay>
 """);
@@ -162,6 +163,5 @@ bob.write("""  </Folder>
 """)
 
 bob.close()
-
-    
+img = None
     
